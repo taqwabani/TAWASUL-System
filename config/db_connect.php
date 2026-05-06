dee2b5441bd7272f8bad29863ed8b2cba5592add
taqwaali048 <taqwaali048@gmail.com>
2026-05-06 14:41:11 +0200

    Connect to the database - taqwa

---
+++ config/db_connect.php
@@ -0,0 +1,18 @@
+<?php
+//  الاتصال بقاعدة البيانات
+$host = "localhost";
+$db_name = "tawasul db";
+$username = "root";
+$password = "";
+try {
+    $dsn = "mysql:host=$host;dbname=$db_name;charset=utf8mb4";
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    //echo "تم الاتصال بنجاح";
+
+} catch (PDOException $e) {
+    die("خطأ في الاتصال بقاعدة البيانات: " . $e->getMessage());
+}
+?>
--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<?php include 'config/db_connect.php'; ?>
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <head>
