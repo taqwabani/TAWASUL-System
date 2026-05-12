53fb53e4e531c962d4e5dfbef846ae71f08a81b6
shaimaaatalkag <sha.atallah@out.edut.ly>
2026-05-12 19:24:54 +0200

    backup before pull

---
+++ models/perent.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'User.php';
+include 'Inquiry.php';
+
+//يمثل مستخدم من نوع "ولي أمر
+//  لخصائص   User  يرث من كلاس 
+
+class ParentUser extends User { 
+    
+    //دالة إرسال استفسار
+    //تعمل كوسيط بين الواجهة وبين كلاس الاستفسار لتنفيذ عمليةالارسال
+    public function sendInquiry($db, $subject, $messageText, $userID) {
+        $inquiry = new Inquiry($db);
+        //القادم من الصفحة مباشرةuserID اخد 
+        return $inquiry->create($userID, $subject, $messageText);
+    }
+}
+
+?>
